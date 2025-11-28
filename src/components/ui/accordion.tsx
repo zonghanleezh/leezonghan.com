0af13a20423612fd739289e4c5d9d7dabed6a215
zonghanleezh <zonghanleezh@gmail.com>
2025-11-28 18:22:36 +0800

    fix: update chvron colour to text-background

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -40,7 +40,7 @@ function AccordionTrigger({
         )}
         {...props}>
         {children}
-        <ChevronDownIcon className="pointer-events-none size-4 shrink-0 translate-y-0.5 text-muted-foreground transition-transform duration-200" />
+        <ChevronDownIcon className="pointer-events-none size-4 shrink-0 translate-y-0.5 text-background transition-transform duration-200" />
       </AccordionPrimitive.Trigger>
     </AccordionPrimitive.Header>
   )
